084ad6226f916a0e65a67cb021a589c67724be93
jphamada <jphamada@users.noreply.github.com>
2026-07-07 20:58:00 -0300

    Admin updates: Removed admin links from front-end, added iframe code generator, and caught Supabase bucket error

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // Reemplaza los siguientes valores con las credenciales de tu proyecto de Supabase
 // Puedes encontrar estos valores en: Settings -> API de tu panel de Supabase.
 
-const SUPABASE_URL = ""; 
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_URL = "https://unwxalpnlcrhgdohraet.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVud3hhbHBubGNyaGdkb2hyYWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NTk0MjQsImV4cCI6MjA5OTAzNTQyNH0.n_3QfhNDx_eeJwb9-BWtmW3sbMCBt2tIzg7vUQfMNS0";
 
 // Si dejas las variables vacías (""), la aplicación entrará automáticamente en 
 // modo "Local Fallback" utilizando localStorage para almacenar partidos y votos.
